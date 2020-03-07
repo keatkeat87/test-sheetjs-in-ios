225f23f86f50d5a8816bc83c59d76e0b7951a98a
兴杰 严 <keatkeat87@live.com>
2020-03-07 09:27:56 +0800

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,19 +13,18 @@ type AOA = any[][];
 export class AppComponent {
   title = 'sheetjs';
 
-  data: AOA = [['name', 'age', 'DOB', 'isMan'], ['keatkeat', 11, new Date(1987, 11, 15), true]];
+  data: AOA = [['name', 'age', 'DOB', 'isMan'], ['keatkeat', 11, new Date(1987, 11, 15, 3, 4, 22), true]];
   wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
   fileName = 'SheetJS.xlsx';
 
   export(): void {
-    /* generate worksheet */
     const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
-
-    /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
-    /* save to file */
+    const a =  ws.A2;
+    console.log(a);
+
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
     XLSX.writeFile(wb, this.fileName);
   }
 
